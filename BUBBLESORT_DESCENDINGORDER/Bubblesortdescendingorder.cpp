2bddf3e2778b48c8d665b5cb3ebc2cdd14de3708
divya boppudi <divya.rajyalakshmi@gmai.com>
2024-02-04 19:53:32 -0500

    first commit

---
+++ BUBBLESORT_DESCENDINGORDER/Bubblesortdescendingorder.cpp
@@ -0,0 +1,31 @@
+// Title: Bubble Sort Algorithm Implementation in C++ (Descending Order)
+
+#include <iostream>  // Including the input/output stream library for console I/O
+
+int main() {
+    // Initialize an array with integer values
+    int a[] = { 8, 7, 6, 5, 2, 4, 1, 3, 9 };
+
+    // Calculate the length of the array
+    int arrayLength = sizeof(a) / sizeof(a[0]);
+
+    // Bubble sort algorithm to sort the array in descending order
+    for (int i = 0; i < arrayLength - 1; i++) {
+        // Iterate through the array up to the last but one element
+        for (int j = 0; j < arrayLength - 1 - i; j++) {
+            // Compare adjacent elements and swap if necessary to sort in descending order
+            if (a[j] < a[j + 1]) {
+                std::swap(a[j], a[j + 1]); // Use std::swap for simplicity
+            }
+        }
+    }
+
+    // Print the sorted array in descending order
+    std::cout << "Sorted Array (Descending Order): ";
+    for (int i = 0; i < arrayLength; i++) {
+        std::cout << a[i] << " ";   // Output each element of the sorted array
+    }
+
+    // Indicating successful completion of the program
+    return 0;
+}
---
+++ DSA_QUEUE/QUEUE.cpp
@@ -0,0 +1,57 @@
+#include <iostream>           //To use cout, include the input/output stream library.
+#include <queue>              //To use the queue data structure, include the queue library.
+
+using namespace std;          //To make code simpler, use the standard namespace 
+
+
+void showq(queue<int> gq)      //Create a function called "showq" that accepts an integer queue as a parameter.
+
+{
+
+	queue<int> g = gq;     //Make a replica of the input queue 'gq' named 'g'.
+	while (!g.empty())      //Loop until the queue 'g' is empty.
+	{
+		cout << '\t' << g.front();  //Print the front element of the queue with a tab character.
+		g.pop();        //Remove the front element from the queue.
+	}
+
+	cout << '\n';                    //Print a newline character after printing all elements.
+
+}
+
+
+
+// The main function where the execution of the program begins.
+int main()
+{
+
+	queue<int> gquiz;         //Declare the 'gquiz' integer queue.
+
+	gquiz.push(10);           //Push the value 10 to the back of the queue.
+
+	gquiz.push(20);            //Push the value 20 to the back of the queue.
+
+	gquiz.push(30);            //Push the value 30 to the back of the queue.
+
+	cout << "The queue gquiz is : ";     //Print a message indicating the queue is being displayed
+
+	showq(gquiz);               //To see the queue's contents, use the'showq' function.
+
+
+
+	cout << "\ngquiz.size() : " << gquiz.size();   //Use the'size' member function to print the queue's size.
+
+	cout << "\ngquiz.front() : " << gquiz.front();  //Print the front element of the queue using 'front' member function.
+
+	cout << "\ngquiz.back() : " << gquiz.back();    //Print the back element of the queue using 'back' member function.
+
+
+
+	cout << "\ngquiz.pop() : ";         //Print a message to indicate that an element has been removed from the queue.
+
+	gquiz.pop();                        //Remove the front element from the queue.
+
+	showq(gquiz);                       //Call the 'showq' function to display the updated elements of the queue.
+	return 0;                           // Indicate successful execution of the program.
+
+}
